e517fa6bcd536c0d7e33f73300ccb35663e09796
Brian L. Troutwine <brian@troutwine.us>
2015-10-10 00:32:31 -0500

    Add "overseer:sustain_workers/1" to the system.
    This addition gives the overseer the ability to create and keep a
    certain number of workers on and active in the system.
    
    Signed-off-by: Brian L. Troutwine <brian@troutwine.us>

--- apps/overseer/src/overseer.erl
+++ apps/overseer/src/overseer.erl
@@ -1,6 +1,6 @@
 -module(overseer).
 
--export([start_workers/1]).
+-export([start_workers/1, sustain_workers/1]).
 
 %%%===================================================================
 %%% API
@@ -8,6 +8,9 @@
 
 -spec start_workers(N :: non_neg_integer()) -> ok.
 start_workers(N) -> overseer_srv:start_workers(N).
+
+-spec sustain_workers(N :: non_neg_integer()) -> ok.
+sustain_workers(N) -> overseer_srv:sustain_workers(N).
 
 %%%===================================================================
 %%% Internal Functions
--- apps/overseer/src/overseer_srv.erl
+++ apps/overseer/src/overseer_srv.erl
@@ -3,13 +3,14 @@
 -behaviour(gen_server).
 
 %% API
--export([start_link/0, start_workers/1]).
+-export([start_link/0, start_workers/1, sustain_workers/1]).
 
 %% gen_server callbacks
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
          terminate/2, code_change/3]).
 
 -record(state, {
+          sustain       = 0 :: non_neg_integer(),
           total_workers = 0 :: non_neg_integer()
          }).
 
@@ -24,7 +25,10 @@ start_link() ->
     gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
 start_workers(N) when is_integer(N) ->
-    gen_server:call(?MODULE, {create, N}, timer:seconds(5)).
+    gen_server:call(?MODULE, {create, 0, N}, timer:seconds(5)).
+
+sustain_workers(N) when is_integer(N) ->
+    gen_server:call(?MODULE, {create, N, N}, timer:seconds(5)).
 
 %%%===================================================================
 %%% gen_server callbacks
@@ -34,19 +38,26 @@ init([]) ->
     process_flag(trap_exit, true),
     {ok, #state{}}.
 
-handle_call({create, N}, _From, State = #state{total_workers = TW}) ->
-    lists:foreach(fun(_) ->
-                          {ok, _Pid} = overseer_wrkr:start_link()
-                  end, lists:seq(1, N)),
-    {reply, ok, State#state{total_workers = TW+N}};
+handle_call({create, Sus, N}, _From, State = #state{sustain = S, total_workers = TW}) ->
+    create_workers(N),
+    {reply, ok, State#state{sustain = S + Sus, total_workers = TW+N}};
 handle_call(_Request, _From, State) ->
     {reply, ok, State}.
 
 handle_cast(_Msg, State) ->
     {noreply, State}.
 
-handle_info({'DOWN', _Ref, process, _Pid, work_completed}, State = #state{total_workers=TW}) ->
-    {noreply, State#state{total_workers = TW-1}};
+handle_info({'EXIT', _Pid, normal},
+            State = #state{sustain = S, total_workers=TW}) ->
+    NewTW = TW-1,
+    NewState = case NewTW < S of
+                   true ->
+                       create_workers(S - NewTW),
+                       State#state{total_workers = S};
+                   false ->
+                       State#state{total_workers = NewTW}
+               end,
+    {noreply, NewState};
 handle_info(_Info, State) ->
     {noreply, State}.
 
@@ -59,3 +70,8 @@ code_change(_OldVsn, State, _Extra) ->
 %%%===================================================================
 %%% Internal functions
 %%%===================================================================
+
+create_workers(N) when N > 0 ->
+    lists:foreach(fun(_) ->
+                          {ok, _Pid} = overseer_wrkr:start_link()
+                  end, lists:seq(1, N)).
